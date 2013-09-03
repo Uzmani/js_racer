739f7bb55f71e31f66cc813a9afacbea354b825a
Apprentice <apprentice@devbootcamp.com>
2013-09-03 15:55:47 -0700

    done

--- application.js
+++ application.js
@@ -3,17 +3,24 @@ var player2Position = 0;
 $(document).ready(function() {
   $(document).on('keyup', function(event) {
     // Detect which key was pressed and call the appropriate function
-    var player1Position = 0;
-    var player2Position = 0;
+    var boardLength = 30;
     if (event.keyCode == 80) {
-      console.log("you pressed the letter p");
       $("#player1_strip").find(".active").removeClass()
+      .next().addClass("active");
+
+      if(player1Position >= boardLength) {
+        alert("Player 1 wins!")
+      } 
+      player1Position++;
 
     };
     if (event.keyCode == 81) {
-      console.log("you pressed the letter q");
       $("#player2_strip").find(".active").removeClass()
-
+      .next().addClass("active");
+      if(player2Position >= boardLength) {
+        alert("Player 2 wins!")
+      } 
+      player2Position++;
     };
 
   });
